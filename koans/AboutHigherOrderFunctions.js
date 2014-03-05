e9ae3f47bb2fb6d6f845245530327c9ea2b362d3
zzmp <zmp@umich.edu>
2014-03-05 09:22:35 -0600

    Knowing it should use chain() ... .value() to use multiple higher order functions. Meditated on Higher Order Functions

--- koans/AboutHigherOrderFunctions.js
+++ koans/AboutHigherOrderFunctions.js
@@ -88,7 +88,7 @@ describe("About Higher Order Functions", function () {
                      .reduce(function (sum, x) { return sum + x })
                      .value();
 
-    expect(result).toEqual(FILL_ME_IN);
+    expect(result).toEqual(6);
   });
 
 });
