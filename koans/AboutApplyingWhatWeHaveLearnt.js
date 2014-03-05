ed9982d609a0d7963cb176d3c56a9b3207cb02ac
zzmp <zmp@umich.edu>
2014-03-05 12:31:45 -0600

    Knowing it should add all the natural numbers below 1000 that are multiples of 3 or 5.

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -59,14 +59,17 @@ describe("About Applying What We Have Learnt", function() {
       }
     }
     
-    expect(sum).toBe(FILL_ME_IN);
+    expect(sum).toBe(233168);
   });
 
   it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (functional)", function () {
 
-    var sum = FILL_ME_IN;    /* try chaining range() and reduce() */
+    var sum = 0;
+    var sum = _(_.range(0,1000)).chain()
+                 .reduce(function (sum, x) { return (x % 3 === 0 || x % 5 === 0) ? sum + x : sum; })
+                 .value();    /* try chaining range() and reduce() */
 
-    expect(233168).toBe(FILL_ME_IN);
+    expect(233168).toBe(sum);
   });
 
   /*********************************************************************************/
